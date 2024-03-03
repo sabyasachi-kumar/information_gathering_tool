04b8124c97cf9f44ca34c5d00dc94685ada647c8
sabyasachi-kumar <122230369+sabyasachi-kumar@users.noreply.github.com>
2024-03-03 15:10:25 +0530

    Add files via upload

---
+++ info.py
@@ -0,0 +1,17 @@
+import sys
+import requests
+import socket
+import json
+if len(sys.argv) < 2:
+    print("Usage: " + sys.argv[0] + " <url>")
+    sys.exit(1)
+req = requests.get("https://" + sys.argv[1])
+print("\n"+str(req.headers))
+gethostby_ = socket.gethostbyname(sys.argv[1])
+print("\nThe IP address of " +sys.argv[1]+ " is: " +gethostby_ +"\n")
+
+req_two = requests.get("https://ipinfo.io/"+gethostby_+"/json")
+resp_ = json.loads(req_two.text)
+
+print("location: "+resp_["loc"])
+print("region: " + resp_["region"])
